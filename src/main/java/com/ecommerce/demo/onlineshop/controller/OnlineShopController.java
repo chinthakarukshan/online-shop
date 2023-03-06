455778519cede5c2fb68ec75d3a3236abbee82c9
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2023-03-06 23:58:23 +0530

    Refactored controller class name

--- src/main/java/com/ecommerce/demo/onlineshop/controller/OnlineShopController.java
+++ src/main/java/com/ecommerce/demo/onlineshop/controller/OnlineShopController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/onlineshop")
-public class OnlineShopContoller {
+public class OnlineShopController {
 
 
 }
