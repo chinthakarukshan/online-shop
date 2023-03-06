20461388a7713d836df01ed8a517755439e3ce74
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2023-03-06 23:48:34 +0530

    Initial commit for online shopping application

---
+++ src/test/java/com/ecommerce/demo/onlineshop/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.demo.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
