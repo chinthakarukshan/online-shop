b8edbf3219fff6f0256c6e1334f3054d0d955cb0
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2023-03-18 00:59:37 +0530

    Enabled Spring Resource Server

---
+++ src/main/java/com/ecommerce/demo/onlineshop/configuration/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.ecommerce.demo.onlineshop.configuration;
+
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors()
+            .and()
+            .authorizeHttpRequests((authz) -> authz.anyRequest()
+                                                   .permitAll())
+            .oauth2ResourceServer()
+            .jwt();
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
+        //return JwtDecoders.fromIssuerLocation("https://api.asgardeo.io/t/chinthakaorg/oauth2/token");
+        return NimbusJwtDecoder.withJwkSetUri(properties.getJwt().getJwkSetUri())
+                               .jwtProcessorCustomizer(jwtCustomizer -> {
+                                   jwtCustomizer.setJWSTypeVerifier(new DefaultJOSEObjectTypeVerifier<>(new JOSEObjectType("at+jwt")));
+                               })
+                               .build();
+    }
+}
